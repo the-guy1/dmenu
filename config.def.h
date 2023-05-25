a24156087923657db585109127a3e5212df9f7bb
the-guy1 <koodog@gmail.com>
2023-05-25 11:39:47 -0400

    Going Catppuccin

--- config.def.h
+++ config.def.h
@@ -3,7 +3,7 @@
 
 static int topbar = 1; /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {"JatBrainsMono Nerd Font:size=11"};
+static const char *fonts[] = {"JetBrainsMono Nerd Font:size=11"};
 static const char *prompt =
     NULL; /* -p  option; prompt to the left of input field */
 // static const char *colors[SchemeLast][2] = {
@@ -14,8 +14,13 @@ static const char *prompt =
 //      [SchemeNormHighlight] = {"#ffc978", "#222222"}, */
 //     [SchemeOut] = {"#000000", "#00ffff"},
 // };
-#include "/home/ken/.cache/wal/colors-wal-dmenu.h"
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#cdd6f4", "#1e1e2e" },
+	[SchemeSel] = { "#1e1e2e", "#89dceb" },
+	[SchemeOut] = { "#000000", "#89dceb" },
+};
 static unsigned int lines = 0;
 
 /*
